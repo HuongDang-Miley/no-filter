3df2434b374a17fa3648b0713b0049adc60b49f8
Huong Dang <miley.dawn@codeimmersives.com>
2020-07-07 12:10:18 -0400

    submit homework

--- main.js
+++ main.js
@@ -1,6 +1,50 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const onlyOdds = function (arr) {
+  let result = []
+  for (const n of arr) {
+    if (n % 2 === 1) {
+      result.push(n)
+    }
+  }
+  return result
+}
+
+
+
+const onlyEvens = function (arr) {
+  let result = []
+  for (const n of arr) {
+    if (n % 2 === 0) {
+      result.push(n)
+    }
+  } return result
+}
+
+
+
+const shortNamesOnly = function (arr) {
+  let result = []
+  for (const name of arr) {
+    if (name.length < 7) {
+      result.push(name)
+    }
+  }
+  return result
+}
+
+
+
+const dNames = function (arr) {
+  let result = []
+  for (const name of arr) {
+    if (name[0].toLowerCase() === 'd') {
+      result.push(name)
+    }
+  }
+  return result
+}
 
 
 
